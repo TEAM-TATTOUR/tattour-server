5807b7b6b3a5c53b7ded3cb7d86936c2b7681102
유서린 <sprout3082@gmail.com>
2023-06-23 18:43:45 +0900

    #1 [init] JwtToken Service추가

--- tattour-server/src/main/java/org/tattour/server/golbal/config/jwt/JwtService.java
+++ tattour-server/src/main/java/org/tattour/server/golbal/config/jwt/JwtService.java
@@ -1,5 +1,82 @@
 package org.tattour.server.golbal.config.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.tattour.server.golbal.exception.ErrorType;
+import org.tattour.server.golbal.exception.UnauthorizedException;
+
+@Service
 public class JwtService {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
+	@PostConstruct
+	protected void init() {
+		jwtSecret = Base64.getEncoder()
+			.encodeToString(jwtSecret.getBytes(StandardCharsets.UTF_8));
+	}
+
+	// JWT 토큰 발급
+	public String issuedToken(Long userId) {
+		String strUserId = String.valueOf(userId);
+		final Date now = new Date();
+
+		// 클레임 생성
+		final Claims claims = Jwts.claims()
+			.setSubject("access_token")
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + 120 * 60 * 1000L));
+
+		//private claim 등록
+		claims.put("userId", strUserId);
+
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE , Header.JWT_TYPE)
+			.setClaims(claims)
+			.signWith(getSigningKey())
+			.compact();
+	}
+
+	private Key getSigningKey() {
+		final byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	// JWT 토큰 검증
+	public boolean verifyToken(String token) {
+		try {
+			final Claims claims = getBody(token);
+			return true;
+		} catch (RuntimeException e) {
+			if (e instanceof ExpiredJwtException) {
+				throw new UnauthorizedException(ErrorType.TOKEN_TIME_EXPIRED_EXCEPTION);
+			}
+			return false;
+		}
+	}
+
+	private Claims getBody(final String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(getSigningKey())
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+	}
+
+	// JWT 토큰 내용 확인
+	public String getJwtContents(String token) {
+		final Claims claims = getBody(token);
+		return (String) claims.get("userId");
+	}
 }
