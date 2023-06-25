2e0993b200cdf1ae4cc239c8d651f50d125e9e99
유서린 <sprout3082@gmail.com>
2023-06-25 14:56:37 +0900

    #1 [init] Profile 확인 Controller

---
+++ server/src/main/java/org/tattour/server/ServerProfileController.java
@@ -0,0 +1,21 @@
+package org.tattour.server;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ServerProfileController {
+
+	private final Environment env;
+
+	@GetMapping("/profile")
+	public String getProfile() {
+		return Arrays.stream(env.getActiveProfiles())
+			.findFirst()
+			.orElse("");
+	}
+}
