5e2dc675a52be5fca0e4be5cb8025f9f4800d8e3
유서린 <sprout3082@gmail.com>
2023-07-09 20:12:25 +0900

    #6 [feat] 테마 엔티티

---
+++ server/src/main/java/org/tattour/server/theme/domain/Theme.java
@@ -0,0 +1,36 @@
+package org.tattour.server.theme.domain;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.tattour.server.custom.domain.CustomStyle;
+import org.tattour.server.custom.domain.CustomTheme;
+import org.tattour.server.sticker.domain.StickerStyle;
+import org.tattour.server.sticker.domain.StickerTheme;
+
+@Table(name = "theme")
+@Entity
+public class Theme {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private String description;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@OneToMany(mappedBy = "theme")
+	private List<StickerTheme> stickerThemes;
+
+	@OneToMany(mappedBy = "theme")
+	private List<CustomTheme> customThemes;
+}
