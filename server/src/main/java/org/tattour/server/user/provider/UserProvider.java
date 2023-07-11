7104cd959c565b599e7a791949dab5a6cdebd9ff
Lee ChanMi <anytime0224@gmail.com>
2023-07-11 16:27:09 +0900

    #11 [feat] user 프로필 정보 가져오기

--- server/src/main/java/org/tattour/server/user/provider/UserProvider.java
+++ server/src/main/java/org/tattour/server/user/provider/UserProvider.java
@@ -1,7 +1,15 @@
 package org.tattour.server.user.provider;
 
+import org.tattour.server.user.domain.User;
+import org.tattour.server.user.service.dto.response.GetUserProfileRes;
+
 public interface UserProvider {
-    // 유저 중복 확인(이메일로)
-    // 존재하면 userId / 존재하지 않으면 null return
+    // 유저 가져오기
+    User getUserByUserId(Integer userId);
+
+    // 유저 프로필 가져오기
+    GetUserProfileRes getUserProfile(Integer userId);
+
+    // 이메일로 유저 중복 확인
     Integer checkDuplicateByEmail(String email);
 }
