f7e92dd79fd255c2f4ea775ed67c37ee9d5546aa
유서린 <sprout3082@gmail.com>
2023-07-14 22:51:06 +0900

    #34 [feat] swagger header 검사 추가

--- server/src/main/java/org/tattour/server/global/config/SwaggerConfig.java
+++ server/src/main/java/org/tattour/server/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package org.tattour.server.global.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+	name = "JWT Auth",
+	type = SecuritySchemeType.HTTP,
+	bearerFormat = "JWT",
+	scheme = "bearer"
+)
 public class SwaggerConfig {
 
 	@Bean
