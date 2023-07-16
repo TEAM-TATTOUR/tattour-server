9a44bac0463e433a4cab7a977468c5e08071c998
유서린 <sprout3082@gmail.com>
2023-07-17 03:28:23 +0900

    #34 [FEAT] DTO 추가

---
+++ server/src/main/java/org/tattour/server/infra/discord/dto/resquest/CustomApplyDiscordMessage.java
@@ -0,0 +1,39 @@
+package org.tattour.server.infra.discord.dto.resquest;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.tattour.server.domain.custom.domain.Custom;
+import org.tattour.server.domain.user.domain.User;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CustomApplyDiscordMessage {
+
+	String title;
+	String description;
+	DiscordCustomImage image;
+
+	public static CustomApplyDiscordMessage from(User user, Custom custom) {
+		String title = "Custom Id : " + custom.getId();
+		String description;
+		if (custom.getHaveDesign()) {
+			description = "이름 : " + custom.getName()
+				+ "\n사이즈 : " + custom.getSize().getSize()
+				+ "\n추가 요청 사항 : " + custom.getDemand()
+				+ "\n수량 : " + custom.getCount();
+		} else {
+			description = "이름 : " + custom.getName()
+				+ "\n사이즈 : " + custom.getSize().getSize()
+				+ "\n추가 요청 사항 : " + custom.getDemand()
+				+ "\n색상 : " + custom.getIsColored()
+				+ "\n주제 및 설명 : " + custom.getDescription()
+				+ "\n수량 : " + custom.getCount();
+		}
+		return new CustomApplyDiscordMessage(
+			title,
+			description,
+			new DiscordCustomImage(custom.getMainImageUrl())
+		);
+	}
+}
