c068fd54e9b9c789840b17a67fa74e4ca8e99930
유서린 <sprout3082@gmail.com>
2023-07-18 03:50:09 +0900

    #47 [DOCS] Theme Controller Swagger 보충

--- server/src/main/java/org/tattour/server/domain/theme/controller/ThemeController.java
+++ server/src/main/java/org/tattour/server/domain/theme/controller/ThemeController.java
@@ -1,16 +1,22 @@
 package org.tattour.server.domain.theme.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tattour.server.domain.style.service.dto.response.StyleSummaryList;
 import org.tattour.server.domain.theme.service.ThemeService;
 import org.tattour.server.domain.theme.service.dto.response.ThemeInfoList;
 import org.tattour.server.domain.theme.service.dto.response.ThemeSummaryList;
 import org.tattour.server.global.dto.BaseResponse;
+import org.tattour.server.global.dto.FailResponse;
 import org.tattour.server.global.dto.SuccessType;
 
 @RestController
@@ -23,6 +29,12 @@ public class ThemeController {
 
 	@GetMapping
 	@Operation(summary = "테마 리스트 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "success",
+			content = @Content(schema = @Schema(implementation = ThemeInfoList.class))),
+		@ApiResponse(responseCode = "400, 500", description = "error",
+			content = @Content(schema = @Schema(implementation = FailResponse.class)))
+	})
 	public ResponseEntity<?> getThemeList() {
 		ThemeInfoList response = themeService.getAllTheme();
 		return BaseResponse.success(SuccessType.READ_ALL_THEME_SUCCESS, response);
@@ -30,6 +42,12 @@ public class ThemeController {
 
 	@GetMapping("/summary")
 	@Operation(summary = "테마 요약 정보 리스트 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "success",
+			content = @Content(schema = @Schema(implementation = ThemeSummaryList.class))),
+		@ApiResponse(responseCode = "400, 500", description = "error",
+			content = @Content(schema = @Schema(implementation = FailResponse.class)))
+	})
 	public ResponseEntity<?> getThemeSummaryList() {
 		ThemeSummaryList response = themeService.getAllThemeSummary();
 		return BaseResponse.success(SuccessType.READ_ALL_THEME_SUMMARY_SUCCESS, response);
