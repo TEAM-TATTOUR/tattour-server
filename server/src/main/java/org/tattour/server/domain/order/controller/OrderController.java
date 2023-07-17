dbade86883f62efb44127a5a99718131f73963aa
유서린 <sprout3082@gmail.com>
2023-07-17 19:23:28 +0900

    #34 [FIX] api 버전 주소 추가, 일반구매 디스코드 알림톡 기능

--- server/src/main/java/org/tattour/server/domain/order/controller/OrderController.java
+++ server/src/main/java/org/tattour/server/domain/order/controller/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tattour.server.domain.order.controller.dto.request.GetOrderSheetReq;
 import org.tattour.server.domain.order.controller.dto.request.PostOrderReq;
+import org.tattour.server.domain.order.domain.Order;
 import org.tattour.server.domain.order.provider.impl.OrderProviderImpl;
 import org.tattour.server.domain.order.service.impl.OrderServiceImpl;
 import org.tattour.server.domain.point.service.dto.request.SaveUserPointLogReq;
@@ -27,9 +28,10 @@ import org.tattour.server.global.dto.ApiResponse;
 import org.tattour.server.global.dto.SuccessType;
 import org.tattour.server.global.exception.BusinessException;
 import org.tattour.server.global.exception.ErrorType;
+import org.tattour.server.infra.discord.service.DiscordMessageService;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "JWT Auth")
 @Tag(name = "Order", description = "Order API Document")
@@ -40,6 +42,7 @@ public class OrderController {
 	private final PointServiceImpl pointService;
 	private final UserProviderImpl userProvider;
 	private final UserServiceImpl userService;
+	private final DiscordMessageService discordMessageService;
 	private final JwtService jwtService;
 
 	@Operation(summary = "결제 페이지 불러오기")
@@ -62,13 +65,13 @@ public class OrderController {
 			throw new BusinessException(ErrorType.LACK_OF_POINT_EXCEPTION);
 		}
 
-		orderService.saveOrder(req);
+		Order order = orderService.saveOrder(req);
 		int resultPoint = userService.updateUserPoint(
 			UpdateUserPointReq.of(req.getUserId(), -Math.abs(req.getTotalAmount())));
 		pointService.savePointLog(
 			SaveUserPointLogReq.of("상품 구매", null, -Math.abs(req.getTotalAmount()), resultPoint,
 				req.getUserId()));
-
+		discordMessageService.sendOrderStickerMessage(order);
 		return ApiResponse.success(SuccessType.CREATE_ORDER_SUCCESS);
 	}
 
