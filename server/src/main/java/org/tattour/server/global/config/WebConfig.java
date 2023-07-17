9b27afb0771b2dd1a6e1bff3cdd87ab3956d167a
Lee ChanMi <anytime0224@gmail.com>
2023-07-18 01:19:53 +0900

    #35 [FIX] CORS 에러 해결

--- server/src/main/java/org/tattour/server/global/config/WebConfig.java
+++ server/src/main/java/org/tattour/server/global/config/WebConfig.java
@@ -3,6 +3,8 @@ package org.tattour.server.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.tattour.server.global.config.resolver.UserIdResolver;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserIdResolver userIdResolver;
@@ -17,5 +20,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userIdResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 앞으로 만들 모든 CORS 정보를 적용한다
+        registry.addMapping("/**")
+                // Header의 Origin에 들어있는 주소가 아래 주소인 경우를 허용한다
+                .allowedOrigins("https://api.tattour.kr", "http://localhost:8080", "http://localhost:5173")
+                .allowedHeaders()
+                // 모든 HTTP Method를 허용한다.
+                .allowedMethods("*")
+                // HTTP 요청의 Header에 어떤 값이든 들어갈 수 있도록 허용한다.
+                .allowedHeaders("*")
+                // 자격증명 사용을 허용한다.
+                // 해당 옵션 사용시 allowedOrigins를 * (전체)로 설정할 수 없다.
+                .allowCredentials(true);
     }
 }
