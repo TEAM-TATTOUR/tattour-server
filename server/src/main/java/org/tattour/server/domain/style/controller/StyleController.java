6a047a0f78c0093e589190f90a9bb2147584a96d
유서린 <sprout3082@gmail.com>
2023-07-18 03:48:18 +0900

    #47 [DOCS] Style Controller Swagger 보충

--- server/src/main/java/org/tattour/server/domain/style/controller/StyleController.java
+++ server/src/main/java/org/tattour/server/domain/style/controller/StyleController.java
@@ -1,6 +1,10 @@
 package org.tattour.server.domain.style.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,7 @@ import org.tattour.server.domain.style.service.StyleService;
 import org.tattour.server.domain.style.service.dto.response.StyleInfoList;
 import org.tattour.server.domain.style.service.dto.response.StyleSummaryList;
 import org.tattour.server.global.dto.BaseResponse;
+import org.tattour.server.global.dto.FailResponse;
 import org.tattour.server.global.dto.SuccessType;
 
 @RestController
@@ -23,6 +28,12 @@ public class StyleController {
 
 	@GetMapping
 	@Operation(summary = "스타일 리스트 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "success",
+			content = @Content(schema = @Schema(implementation = StyleInfoList.class))),
+		@ApiResponse(responseCode = "400, 500", description = "error",
+			content = @Content(schema = @Schema(implementation = FailResponse.class)))
+	})
 	public ResponseEntity<?> getStyleList() {
 		StyleInfoList response = styleService.getAllStyle();
 		return BaseResponse.success(SuccessType.READ_ALL_STYLE_SUCCESS, response);
@@ -30,6 +41,12 @@ public class StyleController {
 
 	@GetMapping("/summary")
 	@Operation(summary = "스타일 요약 정보 리스트 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "success",
+			content = @Content(schema = @Schema(implementation = StyleSummaryList.class))),
+		@ApiResponse(responseCode = "400, 500", description = "error",
+			content = @Content(schema = @Schema(implementation = FailResponse.class)))
+	})
 	public ResponseEntity<?> getStyleSummaryList() {
 		StyleSummaryList response = styleService.getAllStyleSummary();
 		return BaseResponse.success(SuccessType.READ_ALL_STYLE_SUMMARY_SUCCESS, response);
