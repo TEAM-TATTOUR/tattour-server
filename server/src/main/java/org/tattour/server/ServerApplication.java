750f6660742aa6de7a86540fd9dfbe38e1edb82e
Lee ChanMi <anytime0224@gmail.com>
2023-07-19 05:30:37 +0900

    #50 [FIX] swagger api CORS error 해결

--- server/src/main/java/org/tattour/server/ServerApplication.java
+++ server/src/main/java/org/tattour/server/ServerApplication.java
@@ -10,8 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableFeignClients
-//@OpenAPIDefinition(servers = {@Server(url = "https://api.tattour.kr", description = "Default Server URL")})
-@OpenAPIDefinition(servers = {@Server(url = "http://http://43.201.90.237:9000/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "https://api.tattour.shop", description = "Default Server URL")})
 public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
