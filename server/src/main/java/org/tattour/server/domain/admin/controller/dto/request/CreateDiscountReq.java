d04de746c1731d7ca967392e4ff13b9b8b446aab
유서린 <sprout3082@gmail.com>
2023-07-19 22:53:42 +0900

    #56 [FIX] 간격 조정

--- server/src/main/java/org/tattour/server/domain/admin/controller/dto/request/CreateDiscountReq.java
+++ server/src/main/java/org/tattour/server/domain/admin/controller/dto/request/CreateDiscountReq.java
@@ -1,5 +1,6 @@
 package org.tattour.server.domain.admin.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,9 +13,11 @@ import org.tattour.server.domain.discount.service.dto.request.DiscountInfo;
 public class CreateDiscountReq {
 
 	@NotNull
+	@Schema(description = "할인 정책 이름", example = "Open Event!")
 	private String name;
 
 	@Range(min = 0, max = 100)
+	@Schema(description = "할인률 : 0~100")
 	private Integer discountRate;
 
 	@NotNull
