d5ad13c8d681571fd3dde2cee6a165d8730dec96
Lee ChanMi <anytime0224@gmail.com>
2023-12-06 02:07:21 +0900

    [FIX] 일반 결제 후 디스코드 메시지 임시 응답으로 변경

--- server/src/main/java/org/tattour/server/infra/discord/dto/resquest/OrderStickerDiscordMessage.java
+++ server/src/main/java/org/tattour/server/infra/discord/dto/resquest/OrderStickerDiscordMessage.java
@@ -3,7 +3,7 @@ package org.tattour.server.infra.discord.dto.resquest;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.tattour.server.domain.order.domain.Order;
+import org.tattour.server.domain.order.domain.OrderHistory;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,25 +13,31 @@ public class OrderStickerDiscordMessage {
     String description;
     DiscordCustomImage image;
 
-    public static OrderStickerDiscordMessage from(Order order) {
-        String title = "Order Id : " + order.getId();
-        String description = "\n유저 Id : " + order.getUser().getId()
-                + "\n수령인 : " + order.getRecipientName()
-                + "\n연락처 : " + order.getContact()
-                + "\n유저 이메일 : " + order.getMailingAddress()
-                + "\n상품 Id : " + order.getProductName()
-                + "\n상품 이름 : " + order.getProductName()
-                + "\n상품 크기 : " + order.getProductSize()
-                + "\n상품 금액 : " + order.getProductAmount()
-                + "\n상품 개수 : " + order.getProductCount()
-                + "\n총 주문 금액 : " + order.getTotalAmount()
-                + "\n기본 주소 : " + order.getBaseAddress()
-                + "\n싱세 주소 : " + order.getDetailAddress()
-                + "\n배송비 : " + order.getShippingFee();
+    // todo: 디스코드 메시지 내용 형식 수정
+    public static OrderStickerDiscordMessage from(OrderHistory orderHistory) {
+//        String title = "Order Id : " + orderHistory.getId();
+//        String description = "\n유저 Id : " + orderHistory.getUser().getId()
+//                + "\n수령인 : " + orderHistory.getRecipientName()
+//                + "\n연락처 : " + orderHistory.getContact()
+//                + "\n유저 이메일 : " + orderHistory.getMailingAddress()
+//                + "\n상품 Id : " + orderHistory.getProductName()
+//                + "\n상품 이름 : " + orderHistory.getProductName()
+//                + "\n상품 크기 : " + orderHistory.getProductSize()
+//                + "\n상품 금액 : " + orderHistory.getProductAmount()
+//                + "\n상품 개수 : " + orderHistory.getProductCount()
+//                + "\n총 주문 금액 : " + orderHistory.getTotalAmount()
+//                + "\n기본 주소 : " + orderHistory.getBaseAddress()
+//                + "\n싱세 주소 : " + orderHistory.getDetailAddress()
+//                + "\n배송비 : " + orderHistory.getShippingFee();
+//        return new OrderStickerDiscordMessage(
+//                title,
+//                description,
+//                new DiscordCustomImage(orderHistory.getProductImageUrl())
+//        );
         return new OrderStickerDiscordMessage(
-                title,
-                description,
-                new DiscordCustomImage(order.getProductImageUrl())
+                "[임시 응답]",
+                "메시지 형식에 대한 추가 개발이 필요해 임시 응답을 전달합니다.",
+                new DiscordCustomImage(null)
         );
     }
 }
