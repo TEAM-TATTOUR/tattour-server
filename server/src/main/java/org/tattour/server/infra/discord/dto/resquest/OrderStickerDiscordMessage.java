e872168f3cbc3e21589057fe1f74ffc589370da4
05AM <anytime0224@gmail.com>
2024-02-22 02:07:53 +0900

    #156 [REFACTOR] 커스텀 도안 신청, 일반 상품 구매 시 디스코드 메시지 형식 수정

--- server/src/main/java/org/tattour/server/infra/discord/dto/resquest/OrderStickerDiscordMessage.java
+++ server/src/main/java/org/tattour/server/infra/discord/dto/resquest/OrderStickerDiscordMessage.java
@@ -19,27 +19,28 @@ public class OrderStickerDiscordMessage {
             List<OrderedProduct> orderedProducts) {
         String title = "주문 번호: " + orderHistory.getId();
         StringBuilder description = new StringBuilder();
+
         description.append("\n\n### [주문 정보]")
-                .append("\n유저 번호: ").append(orderHistory.getUser().getId())
-                .append("\n상품 금액: ").append(orderHistory.getProductAmount())
-                .append("\n총 주문 금액: ").append(orderHistory.getTotalAmount());
+                .append("\n- 유저 번호: ").append(orderHistory.getUser().getId())
+                .append("\n- 상품 금액: ").append(orderHistory.getProductAmount())
+                .append("\n- 총 주문 금액: ").append(orderHistory.getTotalAmount());
 
         description.append("\n\n### [주문 상품 내역]");
         orderedProducts.forEach(product -> {
             description.append("\n");
-            description.append("\n상품 번호: ").append(product.getSticker().getId());
-            description.append("\n상품명: ").append(product.getSticker().getName());
-            description.append("\n가격: ").append(product.getPrice());
-            description.append("\n수량: ").append(product.getCount());
+            description.append("\n- 상품 번호: ").append(product.getSticker().getId());
+            description.append("\n- 상품명: ").append(product.getSticker().getName());
+            description.append("\n- 가격: ").append(product.getPrice());
+            description.append("\n- 수량: ").append(product.getCount());
         });
 
         description.append("\n\n### [배송 정보]")
-                .append("\n수령인: ").append(orderHistory.getRecipientName())
-                .append("\n연락처: ").append(orderHistory.getContact())
-                .append("\n우편 번호: ").append(orderHistory.getMailingAddress())
-                .append("\n기본 주소: ").append(orderHistory.getBaseAddress())
-                .append("\n싱세 주소: ").append(orderHistory.getDetailAddress())
-                .append("\n배송비: ").append(orderHistory.getShippingFee());
+                .append("\n- 수령인: ").append(orderHistory.getRecipientName())
+                .append("\n- 연락처: ").append(orderHistory.getContact())
+                .append("\n- 우편 번호: ").append(orderHistory.getMailingAddress())
+                .append("\n- 기본 주소: ").append(orderHistory.getBaseAddress())
+                .append("\n- 싱세 주소: ").append(orderHistory.getDetailAddress())
+                .append("\n- 배송비: ").append(orderHistory.getShippingFee());
 
         return new OrderStickerDiscordMessage(title, description.toString());
     }
